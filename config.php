ab3728b3fdec287344afbb4bac80d7da4de9e0f9
Said Nassar <93076162+misheo@users.noreply.github.com>
2025-08-31 20:58:31 +0300

    update

--- config.php
+++ config.php
@@ -13,7 +13,7 @@ class Config {
     const SERVER_URL = 'https://your-domain.com';
     
     // Enable/disable MAC address checking
-    const CHECK_MAC_ADDRESS = false; // Set to true if you want MAC verification
+    const CHECK_MAC_ADDRESS = true; // Set to true if you want MAC verification
 }
 
 // Set timezone
